7184fbb1c70e18aa84fd585ac9aa171cfbb2288d
André Hopf <andre.hopf@gmail.com>
2022-12-04 16:05:19 -0300

    Estrutura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { IngredientComponent } from './components/shopping-list/ingredient/ingredient.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipeListComponent } from "./components/recipes/recipe-list/recipe-list.component";
+import { RecipeItemComponent } from "./components/recipes/recipe-list/recipe-item/recipe-item.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    IngredientComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
