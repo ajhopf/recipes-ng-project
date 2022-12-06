6606600bcedd5cd3c5a2088b31df48e434bbe4ae
André Hopf <andre.hopf@gmail.com>
2022-12-06 15:01:46 -0300

    Componentes recipe-list e recipe-item funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { IngredientComponent } from './components/shopping-list/ingredient/ingredient.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { RecipeListComponent } from "./components/recipes/recipe-list/recipe-list.component";
 import { RecipeItemComponent } from "./components/recipes/recipe-list/recipe-item/recipe-item.component";
+import { RecipeDetailComponent } from "./components/recipes/recipe-detail/recipe-detail.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    IngredientComponent,
     HeaderComponent,
     RecipesComponent,
+    RecipeDetailComponent,
     RecipeListComponent,
     RecipeItemComponent
   ],
---
+++ src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent {
+ @Input() recipeName: string;
+ @Input() recipeDescription: string;
+ @Input() recipePath: string;
+ @Input() index: number;
+ @Input() lastIndex: number;
+
+ setItemBorder(index: number) {
+   if (index === 0) {
+     return 'rounded-top'
+   }
+   if (index === this.lastIndex) {
+     return 'rounded-bottom'
+   }
+   return 'rounded-0'
+ }
+}
