6c60c2a7cc53406589a6a4f199342387a7fbaa0c
André Hopf <andre.hopf@gmail.com>
2022-12-12 09:39:53 -0300

    Using event binding and ngif to add navigation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+	@Output() chosenPage = new EventEmitter<string>();
+	pageToRender = 'recipes'
 
+	onRecipesClick() {
+		this.pageToRender = 'recipes'
+		this.chosenPage.emit('recipes')
+	}
+
+	onShoppingListClick() {
+		this.pageToRender = 'shopping'
+		this.chosenPage.emit('shopping')
+	}
 }
--- src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -6,19 +6,19 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent {
- @Input() recipeName: string;
- @Input() recipeDescription: string;
- @Input() recipePath: string;
- @Input() index: number;
- @Input() lastIndex: number;
+  @Input() recipeName: string;
+  @Input() recipeDescription: string;
+  @Input() recipePath: string;
+  @Input() index: number;
+  @Input() lastIndex: number;
 
- setItemBorder(index: number) {
-   if (index === 0) {
-     return 'rounded-top'
-   }
-   if (index === this.lastIndex) {
-     return 'rounded-bottom'
-   }
-   return 'rounded-0'
- }
+  setItemBorder(index: number) {
+    if (index === 0) {
+      return 'rounded-top'
+    }
+    if (index === this.lastIndex) {
+      return 'rounded-bottom'
+    }
+    return 'rounded-0'
+  }
 }
